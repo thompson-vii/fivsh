d4e3401bdb823e8f1e35384a76631cd9dc876f13
Kolloom <kolloom@hotmail.com>
2022-05-30 01:58:58 -0500

    Add logging to image_preproc.py

--- image_preproc.py
+++ image_preproc.py
@@ -1,27 +1,34 @@
-import os
-import imagesize
 from pathlib import Path
 import json
+import logging
+import os
 
+import imagesize
+
+logging.basicConfig(level=logging.INFO)
 
 # list of (post_name, post_dir, dir_fullres, dir_thumbnail)
 PROC_DIR = [("environment", "./portfolio/", "./img/portfolio/", "./img200/portfolio/"),
             ]
-
 OUTPUT_DIR = "./_data/"
-
 EXT = ('*.jpg', '*.png', '*.jpeg')
 
 json_shit = []
 
+logging.info(f"To process {PROC_DIR}")
+logging.info(f"Output to {OUTPUT_DIR}")
+logging.info(f"For extensions {EXT}")
+
 def get_images(file_path, extensions):
+    logging.info(f"Getting file {file_path} matching {extensions}")
+
     files = []
     for ext in extensions:
         files.extend(Path(file_path).glob(ext))
     return files
 
 for shit in PROC_DIR:
-    print(shit)
+    logging.info(f"Iterating through {shit}")
     post_name, post_dir, dir_fullres, dir_thumbnail = shit
     images = get_images(dir_fullres, EXT)
     for image in images:
@@ -36,5 +43,10 @@ for shit in PROC_DIR:
             'height': img_height
         }))
 
+        logging.info(f"Appending {json_shit[-1]}")
+
     with open(os.path.join(OUTPUT_DIR, post_name + '.json'), 'w') as f_out:
+        logging.info(f"Writting json to {f_out}")
         json.dump(json_shit, f_out, indent=4)
+        logging.info(f"Finished")
+       
