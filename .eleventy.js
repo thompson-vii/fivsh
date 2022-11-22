dec91e299da6ed83f7a306ea271aaa4806bd6c0f
Kolloom <kolloom@hotmail.com>
2022-11-22 15:41:43 -0600

    So i guess photoswipe suddenly works now? wtf

--- .eleventy.js
+++ .eleventy.js
@@ -65,7 +65,10 @@ module.exports = function(eleventyConfig) {
   // Copy the `img` and `css` folders to the output
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
-  eleventyConfig.addPassthroughCopy("photoswipe");
+
+  eleventyConfig.addPassthroughCopy({
+    "node_modules/photoswipe/": "/photoswipe/"
+  });
   eleventyConfig.addPassthroughCopy("img200");
   eleventyConfig.addPassthroughCopy("icon");
 
