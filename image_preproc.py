bb90f48e630adc099fabc69da7487c74a8e7c1f5
Kolloom <kolloom@hotmail.com>
2023-02-23 02:01:53 -0600

    Script: improve script to handle multiple portfolio directories

--- image_preproc.py
+++ image_preproc.py
@@ -1,52 +1,88 @@
-from pathlib import Path
+from pathlib import Path, PosixPath
 import json
 import logging
 import os
+from typing import NamedTuple
 
 import imagesize
 
-logging.basicConfig(level=logging.INFO)
+class SwipeDirectory(NamedTuple):
+    json_filename: str
+    dir_fullres: list[PosixPath, ...]
+    dir_thumbnail: list[PosixPath, ...]
+    
+class ImageData():
+    
+    
+    def __init__(self, source: PosixPath):
+        self.source_path = source
+        self.thumbnail_path: PosixPath = self.derive_thumbnail_path(self.source_path)
+        self.filename: str = self.source_path.name
+        self.json_fullres_path: str = "/" + "/".join(self.source_path.parts[-3:])
+        self.json_thumbnail_path: str = "/" + "/".join(self.thumbnail_path.parts[-3:])
+        self.alt_text: str
+        self.width, self.height = imagesize.get(self.source_path)
+    
+    def derive_thumbnail_path(self, source: PosixPath):
+        return Path(str(source).replace("img", "img200"))
 
-# list of (post_name, post_dir, dir_fullres, dir_thumbnail)
-PROC_DIR = [("3drenders", "./portfolio/", "./img/portfolio/", "./img200/portfolio/"),
-            ]
-OUTPUT_DIR = "./_data/"
-EXT = ('*.jpg', '*.png', '*.jpeg')
-
-json_shit = []
-
-logging.info(f"To process {PROC_DIR}")
-logging.info(f"Output to {OUTPUT_DIR}")
-logging.info(f"For extensions {EXT}")
-
-def get_images(file_path, extensions):
-    logging.info(f"Getting file {file_path} matching {extensions}")
-
-    files = []
-    for ext in extensions:
-        files.extend(Path(file_path).glob(ext))
-    return files
-
-for shit in PROC_DIR:
-    logging.info(f"Iterating through {shit}")
-    post_name, post_dir, dir_fullres, dir_thumbnail = shit
-    images = get_images(dir_fullres, EXT)
-    for image in images:
-        img_width, img_height = imagesize.get(image)
+    def get_json(self):
+        return dict({
+            'filename': self.filename,
+            'thumbnail_path': self.json_thumbnail_path, # path need to be relative for the script to work
+            'fullres_path': self.json_fullres_path, # but truncated in production as the cwd is different
+            'alt': "", # optional, read description.yaml
+            'width': self.width,
+            'height': self.height
+        })
         
-        json_shit.append(dict({
-            'filename': image.name,
-            'thumbnail_path': dir_thumbnail[1:] + image.name, # path need to be relative for the script to work
-            'fullres_path': dir_fullres[1:] + image.name, # but truncated in production as the cwd is different
-            'alt': "", # optional, red description.yaml in post_dir
-            'width': img_width,
-            'height': img_height
-        }))
+def get_files(paths: list[PosixPath, ...], extensions: list[str, ...]) -> list[ImageData]:
+    logging.info(f"Getting files in {paths} matching {extensions}")
 
-        logging.info(f"Appending {json_shit[-1]}")
+    image_objects: list[ImageData] = []
+    for dir in paths:
+        for f in dir.iterdir():
+            if f.suffix in EXT:
+                image_objects.append(ImageData(f.resolve()))
+                logging.info(f"{f} found")
 
-    with open(os.path.join(OUTPUT_DIR, post_name + '.json'), 'w') as f_out:
-        logging.info(f"Writting json to {f_out}")
+    return image_objects
+
+def write_json(output_dir: PosixPath, filename: str, json_data):
+    with open(os.path.join(output_dir, filename), 'w') as f_out:
+        logging.info(f"Writting json to {f_out.name}")
         json.dump(json_shit, f_out, indent=4)
         logging.info(f"Finished")
        
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+
+    ROOT = Path("/home/fivsh")
+    OUTPUT_DIR = Path("/home/fivsh/_data/")
+    EXT = ('.jpg', '.png', '.jpeg')
+
+    '''
+    swipe = SwipeDirectory("3drenders.json",
+                       [ROOT.joinpath("img/portfolio"), ROOT.joinpath("img/portfolio_photogrammetry")],
+                       [ROOT.joinpath("img200/portfolio"), ROOT.joinpath("img200/portfolio_photogrammetry")])
+    '''
+    
+    swipe = SwipeDirectory("3drenders.json",
+                       [ROOT.joinpath("img/portfolio")],
+                       [ROOT.joinpath("img200/portfolio")])
+    
+    for p in swipe.dir_fullres:
+        logging.info(f"To process {p}")
+    logging.info(f"Output to {OUTPUT_DIR}")
+    logging.info(f"For extensions {EXT}")
+    
+
+    json_shit = []    
+    images = get_files(swipe.dir_fullres, EXT)
+    for i in images:
+        json_shit.append(i.get_json())
+        
+    print(json_shit)
+    write_json(OUTPUT_DIR, swipe.json_filename, json_shit)
+        
+    
